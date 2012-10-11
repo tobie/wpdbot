e1cf978bca7f3ef39311ac8133ccd8601984da06
Tobie Langel <tobie.langel@gmail.com>
2012-10-11 18:09:48 +0200

    modify syntax

--- README.md
+++ README.md
@@ -5,14 +5,22 @@ This is an very minimal irc bot for the #webplatform channel on freenode. It's b
 
 To summon it just type:
 
-    ![COMMAND] [TARGET]
+    [TARGET]: ![COMMAND] [ARGS]
 
 So for example:
 
-    !faq joe
+    joe: !faq
 
 will print:
 
     Joe: FAQ can be found here: http://docs.webplatform.org/wiki/Site_FAQ.
+
+If the target is omitted, the bot will just use the caller as it's target so for example:
+
+    !faq
+
+will print:
+
+    your_nick: FAQ can be found here: http://docs.webplatform.org/wiki/Site_FAQ.
 
 Adding new commands is easy, just fork this project, modify `./commands.json` and make a pull request.
--- index.js
+++ index.js
@@ -12,30 +12,34 @@ for (k in commands) {
   }
 }
 
+var others = {
+  wpd: function(rest) {
+     u = url.format({
+        protocol: 'http',
+        host: 'docs.webplatform.org',
+        path: 'w/index.php',
+        query: {
+          title: "Special:Search",
+          fulltext: "Search",
+          search: rest
+        }
+      });
+      return 'Here is the result of your search for "' + rest + '": ' + u;
+  }
+};
+
 jerk( function( j ) {
-  j.watch_for(new RegExp("^!(\\S+)(?:\\s+(\\S+))?", 'i'), function(message) {
-    var nick = message.match_data[2] || message.user,
-        cmd = (message.match_data[1] || '').toLowerCase(),
-        msg = commands[cmd];
-    if (msg) {
-      message.say(nick + ": " + msg);
+  j.watch_for(new RegExp("^(?:(\\w+):?)?\\s*!(?:(\\w+))(?:\\s+(.*)$)?", 'i'), function(message) {
+    var nick = message.match_data[1] || message.user,
+        cmd = (message.match_data[2] || '').toLowerCase(),
+        rest = (message.match_data[3] || '').trim();
+    
+    if (others[cmd]) {
+      message.say(nick + ": " + others[cmd](rest));
+    } else if (commands[cmd]) {
+      message.say(nick + ": " + commands[cmd]);
     } else {
       message.say("I'm sorry, " + message.user + ", I do not understand \"" + cmd + "\".");
     }
-  });
-
-  j.watch_for(/^wpd\s+(.*)$/, function(message) {
-    var terms = message.match_data[1];
-    u = url.format({
-      protocol: 'http',
-      host: 'docs.webplatform.org',
-      path: 'w/index.php',
-      query: {
-        title: "Special:Search",
-        fulltext: "Search",
-        search: terms
-      }
-    });
-    message.say(message.user + ', here is the result of your search for "' + terms + '": ' + u);
   });
 }).connect(options);
